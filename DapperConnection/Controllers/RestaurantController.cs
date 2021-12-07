f8f608dce96f891158c57579d37dd38fb7bf96c5
Cortez Christian <CortezChristian@rocketmortgage.com>
2021-12-07 14:49:55 -0500

    Added everything else

---
+++ DapperConnection/Controllers/RestaurantController.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using DapperConnection.Models;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperConnection.Controllers
+{
+    // To makle a controllersubclass controller
+    // .Net expects your controller name to have the word Controller at the end
+    public class RestaurantController : Controller
+    {
+        //Host Name     : 127.0.0.1
+        //UserName      : root
+        //Password      : abc123
+        //Database Name : restaurant
+        //Public static string connection {get; set;} = "server=hostName;uid=user;pwd=blah;database=dbName";
+
+
+        public IActionResult Index()
+        {
+            using(var connect = new MySqlConnection(Secret.Connection))
+            {
+                //So using dapper we are able to pur sql queries into a string
+                //and pass them along to our database
+                //All MySql syntax is useable here
+                string sql = "select * from dishes";
+
+                //open out connection to the DB
+                connect.Open();
+
+                //We need to go and pass thje sql string to the database 
+                //Expecting to return a single model or a list of models or nothing
+                //Depending on the type of query
+                List<Dish> dishes = connect.Query<Dish>(sql).ToList();
+
+                //Close our connection to our DB, if we don't then the database
+                //may not let another method use it
+                connect.Close();
+
+                Dish d = dishes[0];
+                return View(d);
+            }
+            return View();
+        }
+    }
+}
---
+++ DapperConnection/Models/Dish.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperConnection.Models
+{
+/*    ID int NOT NULL auto_increment primary key,
+--     `Name` nvarchar(20) NOT NULL,
+--     Price float NOT NULL,
+--     IsVegetarian bit,
+--     IsHealthy bit,
+--     we want it to be a number between 1 and 10, we need to remember that mysql doesn't enfore max
+--     SpiceLevel int*/
+    public class Dish
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [MaxLength(20, ErrorMessage = "Title must be less than 20 characters")]
+        public string Name { get; set; }
+
+        public float Price { get; set; }
+
+        public bool IsVegetarian { get; set; }
+
+        public bool IsHealthy { get; set; }
+
+        //Range works only on numbers and only allows numbers inclusivlet between it's
+        //min and max
+        [Range(1,10, ErrorMessage = "Spice level needs to be between 1 and 10")]
+        public int SpiceLevel { get; set; }
+    }
+}
